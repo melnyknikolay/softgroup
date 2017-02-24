d6a619f423433aefa053a139d86677f5fc221dd7
melnyknikolay <nikmlk87@gmail.com>
2017-02-24 21:03:44 +0200

    add Roman`s documents authorization api etc.

---
+++ softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/authorization/api/message/RegisterRequest.java
@@ -0,0 +1,39 @@
+package com.softgroup.authorization.api.message;
+
+import com.softgroup.common.protocol.RequestData;
+
+/**
+ * @author odin
+ * @since 20.02.17.
+ */
+public class RegisterRequest implements RequestData{
+	private static final long serialVersionUID = -645554380912935546L;
+
+	private Integer phone_number;
+	private String locale_code;
+	private String device_id;
+
+	public Integer getPhone_number() {
+		return phone_number;
+	}
+
+	public void setPhone_number(Integer phone_number) {
+		this.phone_number = phone_number;
+	}
+
+	public String getLocale_code() {
+		return locale_code;
+	}
+
+	public void setLocale_code(String locale_code) {
+		this.locale_code = locale_code;
+	}
+
+	public String getDevice_id() {
+		return device_id;
+	}
+
+	public void setDevice_id(String device_id) {
+		this.device_id = device_id;
+	}
+}
---
+++ softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/authorization/api/message/RegisterResponse.java
@@ -0,0 +1,39 @@
+package com.softgroup.authorization.api.message;
+
+import com.softgroup.common.protocol.ResponseData;
+
+/**
+ * @author odin
+ * @since 20.02.17.
+ */
+public class RegisterResponse implements ResponseData {
+	private static final long serialVersionUID = -5146888202653750948L;
+
+	private String registration_request_uuid;
+	private Integer registration_timeout_sec;
+	private String auth_code;
+
+	public String getRegistration_request_uuid() {
+		return registration_request_uuid;
+	}
+
+	public void setRegistration_request_uuid(String registration_request_uuid) {
+		this.registration_request_uuid = registration_request_uuid;
+	}
+
+	public Integer getRegistration_timeout_sec() {
+		return registration_timeout_sec;
+	}
+
+	public void setRegistration_timeout_sec(Integer registration_timeout_sec) {
+		this.registration_timeout_sec = registration_timeout_sec;
+	}
+
+	public String getAuth_code() {
+		return auth_code;
+	}
+
+	public void setAuth_code(String auth_code) {
+		this.auth_code = auth_code;
+	}
+}
