6a095b947d3f158557a9f6e1a5b49c57eab4463e
melnyknikolay <nikmlk87@gmail.com>
2017-03-02 22:20:17 +0200

    change common-protocol module

--- softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/Request.java
+++ softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/Request.java
@@ -4,4 +4,35 @@ import java.io.Serializable;
 
 public class Request<T extends Serializable> extends RoutedAction<T> {
 	private static final long serialVersionUID = -1024761631293962969L;
+
+	public Request() {
+	}
+
+	private Request(ActionHeader header, T data) {
+		this.setHeader(header);
+		this.setData(data);
+	}
+
+	public static class Builder<T extends Serializable>{
+
+		private ActionHeader header;
+		private T data;
+
+		public Builder() {
+		}
+
+		public Builder setHeader(ActionHeader header) {
+			this.header = header;
+			return this;
+		}
+
+		public Builder setData(T data) {
+			this.data = data;
+			return this;
+		}
+
+		public Request<T> build() {
+			return new Request<T>(header, data);
+		}
+	}
 }
--- softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/Response.java
+++ softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/Response.java
@@ -7,12 +7,52 @@ public class Response<T extends Serializable> extends RoutedAction<T> {
 
 	private ResponseStatus status;
 
+	public Response() {
+	}
+
+	private Response(ActionHeader header, T data, ResponseStatus status) {
+		this.setHeader(header);
+		this.setData(data);
+		this.status = status;
+	}
+
 	public ResponseStatus getStatus() {
 		return status;
 	}
 
 	public void setStatus(ResponseStatus status) {
 		this.status = status;
+	}
+
+
+
+	public static class Builder<T extends Serializable>{
+
+		private ActionHeader header;
+		private T data;
+		private ResponseStatus status;
+
+		public Builder() {
+		}
+
+		public Builder setHeader(ActionHeader header) {
+			this.header = header;
+			return this;
+		}
+
+		public Builder setData(T data) {
+			this.data = data;
+			return this;
+		}
+
+		public Builder setStatus(ResponseStatus status) {
+			this.status = status;
+			return this;
+		}
+
+		public Response<T> build() {
+			return new Response<T>(header, data, status);
+		}
 	}
 
 }
