0345adfd7e5a7c3ef0113295faaf959da23fdd46
melnyknikolay <nikmlk87@gmail.com>
2017-03-10 00:07:32 +0200

    change common-protocol module

--- softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/ResponseStatus.java
+++ softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/ResponseStatus.java
@@ -2,16 +2,22 @@ package com.softgroup.common.protocol;
 
 import java.io.Serializable;
 
-public class ResponseStatus implements Serializable {
-    private static final long serialVersionUID = 1L;
+public enum ResponseStatus implements Serializable {
+
+    OK(200, "ok"),
+    BAD_REQUEST(400, "Bad request"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not fount"),
+    NOT_ACCEPTABLE(406, "Not acceptable"),
+    UNPROCESABLE_ENTITY(422, "Unprocessable Entity"),
+    TOO_MANY_REQUEST(429, "Too Many Requests"),
+    INTERNAL_SERVER_ERROR(500, "internal server error"),
+    NOT_IMPLEMENTED(501, "not implemented");
 
     private Integer code;
     private String message;
 
-    public ResponseStatus() {
-    }
-
-    public ResponseStatus(Integer code, String message) {
+    ResponseStatus(Integer code, String message) {
         this.code = code;
         this.message = message;
     }
@@ -20,16 +26,9 @@ public class ResponseStatus implements Serializable {
         return code;
     }
 
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
     public String getMessage() {
         return message;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
 
 }
