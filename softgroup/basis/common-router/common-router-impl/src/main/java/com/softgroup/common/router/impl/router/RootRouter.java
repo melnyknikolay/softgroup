b885b43e6ca4e476145b78e90ef2155726d55e47
melnyknikolay <nikmlk87@gmail.com>
2017-03-12 16:56:10 +0200

    change impl

--- softgroup/basis/common-router/common-router-impl/src/main/java/com/softgroup/common/router/impl/router/RootRouter.java
+++ softgroup/basis/common-router/common-router-impl/src/main/java/com/softgroup/common/router/impl/router/RootRouter.java
@@ -1,9 +1,5 @@
 package com.softgroup.common.router.impl.router;
 
-
-import com.softgroup.common.protocol.ProtocolBeansFactory;
-import com.softgroup.common.protocol.Request;
-import com.softgroup.common.protocol.Response;
 import com.softgroup.common.router.api.AbstractRouterHandler;
 import com.softgroup.common.router.api.factory.RootFactory;
 import org.springframework.stereotype.Component;
@@ -17,14 +13,5 @@ public class RootRouter<T extends RootFactory> extends AbstractRouterHandler<T>{
     @Override
     public String getName() {
         return "root";
-    }
-
-    @Override
-    public Response<?> handle(Request<?> msg) {
-        try {
-            return super.handle(msg);
-        }catch (Exception e){
-            return ProtocolBeansFactory.serverError(msg);
-        }
     }
 }
