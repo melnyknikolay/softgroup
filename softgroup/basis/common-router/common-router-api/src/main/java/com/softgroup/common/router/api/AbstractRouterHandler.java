f1dfeaa5e1ffa6b146728662c867dece5952598a
melnyknikolay <nikmlk87@gmail.com>
2017-03-12 16:55:08 +0200

    change api

--- softgroup/basis/common-router/common-router-api/src/main/java/com/softgroup/common/router/api/AbstractRouterHandler.java
+++ softgroup/basis/common-router/common-router-api/src/main/java/com/softgroup/common/router/api/AbstractRouterHandler.java
@@ -1,17 +1,25 @@
 package com.softgroup.common.router.api;
 
+import com.softgroup.common.protocol.ProtocolBeansFactory;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
+import com.softgroup.common.protocol.ResponseStatus;
 import com.softgroup.common.router.api.factory.Factory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class AbstractRouterHandler<T extends Factory> implements RouterHandler {
 
-	@Autowired
-	T factory;
+    @Autowired
+    T factory;
 
-	@Override
-	public Response<?> handle(Request<?> msg) {
-		return factory.getHandler(msg).handle(msg);
-	}
+    @Override
+    public Response<?> handle(Request<?> msg) {
+        try {
+            return factory.getHandler(msg).handle(msg);
+        }catch (NullPointerException e){
+            return ProtocolBeansFactory.getResponse(msg, null, ResponseStatus.NOT_FOUND);
+        }catch (Exception e){
+            return ProtocolBeansFactory.getResponse(msg, null, ResponseStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
